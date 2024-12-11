2ba54fc09e08605a7a4e59a722eca9ba697fae0c
Lim-ji-hun <86521741+Lim-ji-hun@users.noreply.github.com>
2024-12-12 02:00:52 +0900

    리듬게임

---
+++ 기말 과제/test_pygame.py
@@ -0,0 +1,213 @@
+import pygame, math, time, os
+
+pygame.init()
+
+w = 1600
+h = w * (9/16)
+
+screen = pygame.display.set_mode((w, h))
+
+clock = pygame.time.Clock()
+
+main  = True
+ingame = True
+
+keys = [0, 0, 0, 0]
+keyset = [0, 0, 0, 0]
+
+maxframe = 60
+fps = 0
+
+gst = time.time()
+Time = time.time() - gst
+
+ty = 0
+tst = Time
+
+t1 = []
+t2 = []
+t3 = []
+t4 = []
+
+speed = 2
+
+def sum_note(n):
+    ty = 0
+    tst = Time
+    if n == 1:
+        ty = 0
+        tst = Time + 2
+        t1.append([ty, tst])
+    if n == 2:
+        ty = 0
+        tst = Time + 2
+        t2.append([ty, tst])
+    if n == 3:
+        ty = 0
+        tst = Time + 2
+        t3.append([ty, tst])
+    if n == 4:
+        ty = 0
+        tst = Time + 2
+        t4.append([ty, tst])
+        
+
+while main:
+    while ingame:
+
+        Time = time.time() - gst
+
+        fps = clock.get_fps()
+
+        if fps == 0:
+            fps = maxframe
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    keyset[0] = 1
+                    sum_note(1)
+                if event.key == pygame.K_f:
+                    keyset[1] = 1
+                    sum_note(2)
+                if event.key == pygame.K_j:
+                    keyset[2] = 1
+                    sum_note(3)
+                if event.key == pygame.K_k:
+                    keyset[3] = 1
+                    sum_note(4)
+            
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    keyset[0] = 0
+                if event.key == pygame.K_f:
+                    keyset[1] = 0
+                if event.key == pygame.K_j:
+                    keyset[2] = 0
+                if event.key == pygame.K_k:
+                    keyset[3] = 0
+
+        screen.fill((0, 0, 0))
+
+        keys[0] += (keyset[0] - keys[0]) / (2 * (maxframe / fps))
+        keys[1] += (keyset[1] - keys[1]) / (2 * (maxframe / fps))
+        keys[2] += (keyset[2] - keys[2]) / (2 * (maxframe / fps))
+        keys[3] += (keyset[3] - keys[3]) / (2 * (maxframe / fps))
+
+        pygame.draw.rect(screen, (0, 0, 0), (w / 2 - w / 8, -int(w / 100), w / 4, h + int(w / 50)))
+
+        
+        for i in range(7):
+            i += 1
+            pygame.draw.rect(screen, (200 - ((200 / 7) * i), 200 - ((200 / 7) * i), 200 - ((200/ 7) * i)), (w / 2 - w / 8 + w / 32 - (w / 32) * keys[0], (h / 12) * 9 - (h / 30) * keys[0] * i, w / 16 * keys[0], (h / 35) / i)) 
+        for i in range(7):
+            i += 1
+            pygame.draw.rect(screen, (200 - ((200 / 7) * i), 200 - ((200 / 7) * i), 200 - ((200/ 7) * i)), (w / 2 - w / 16 + w / 32 - (w / 32) * keys[1], (h / 12) * 9 - (h / 30) * keys[1] * i, w / 16 * keys[1], (h / 35) / i)) 
+        for i in range(7):
+            i += 1
+            pygame.draw.rect(screen, (200 - ((200 / 7) * i), 200 - ((200 / 7) * i), 200 - ((200/ 7) * i)), (w / 2 + w / 32 - (w / 32) * keys[2], (h / 12) * 9 - (h / 30) * keys[2] * i, w / 16 * keys[2], (h / 35) / i)) 
+        for i in range(7):
+            i += 1
+            pygame.draw.rect(screen, (200 - ((200 / 7) * i), 200 - ((200 / 7) * i), 200 - ((200/ 7) * i)), (w / 2 + w / 16 + w / 32 - (w / 32) * keys[3], (h / 12) * 9 - (h / 30) * keys[3] * i, w / 16 * keys[3], (h / 35) / i)) 
+       
+        """
+
+        for i in range(1, 8):  # i는 1부터 7까지
+            pygame.draw.rect(
+                screen,
+                (
+                    int(200 - ((200 / 7) * i)),
+                    int(200 - ((200 / 7) * i)),
+                    int(200 - ((200 / 7) * i))
+                ),
+                (
+                    w / 2 - w / 8 + w / 32 - (w / 32) * keys[0],
+                    (h / 12) * 9 - (h / 30) * keys[0] * i,
+                    w / 16 * keys[0],
+                    (h / 35) / i
+                )
+            )
+
+        for i in range(1, 8):
+            pygame.draw.rect(
+                screen,
+                (
+                    int(200 - ((200 / 7) * i)),
+                    int(200 - ((200 / 7) * i)),
+                    int(200 - ((200 / 7) * i))
+                ),
+                (
+                    w / 2 - w / 16 + w / 32 - (w / 32) * keys[1],
+                    (h / 12) * 9 - (h / 30) * keys[1] * i,
+                    w / 16 * keys[1],
+                    (h / 35) / i
+                )
+            )
+
+        for i in range(1, 8):
+            pygame.draw.rect(
+                screen,
+                (
+                    int(200 - ((200 / 7) * i)),
+                    int(200 - ((200 / 7) * i)),
+                    int(200 - ((200 / 7) * i))
+                ),
+                (
+                    w / 2 + w / 32 - (w / 32) * keys[2],
+                    (h / 12) * 9 - (h / 30) * keys[2] * i,
+                    w / 16 * keys[2],
+                    (h / 35) / i
+                )
+            )
+
+        for i in range(1, 8):
+            pygame.draw.rect(
+                screen,
+                (
+                    int(200 - ((200 / 7) * i)),
+                    int(200 - ((200 / 7) * i)),
+                    int(200 - ((200 / 7) * i))
+                ),
+                (
+                    w / 2 + w / 16 + w / 32 - (w / 32) * keys[3],
+                    (h / 12) * 9 - (h / 30) * keys[3] * i,
+                    w / 16 * keys[3],
+                    (h / 35) / i
+                )
+            )
+"""
+
+        pygame.draw.rect(screen, (255, 255, 255), (w / 2 - w / 8, -int(w / 100), w / 4, h + int(w / 50)), int(w / 100))
+
+
+        for title_data in t1:
+            title_data[0] = (h / 12) * 9 + (Time - title_data[1]) * 350 * speed * (h/900)
+            pygame.draw.rect(screen, (255, 255, 255), (w /2 - w / 8, title_data[0] - h /100, w / 16, h / 50))
+            if title_data[0] > h - (h / 9):
+                t1.remove(title_data)
+
+        for title_data in t2:
+            title_data[0] = (h / 12) * 9 + (Time - title_data[1]) * 350 * speed * (h/900)
+            pygame.draw.rect(screen, (255, 255, 255), (w /2 - w / 16, title_data[0] - h /100, w / 16, h / 50))
+            if title_data[0] > h - (h / 9):
+                t2.remove(title_data)
+
+        for title_data in t3:
+            title_data[0] = (h / 12) * 9 + (Time - title_data[1]) * 350 * speed * (h/900)
+            pygame.draw.rect(screen, (255, 255, 255), (w /2, title_data[0] - h /100, w / 16, h / 50))
+            if title_data[0] > h - (h / 9):
+                t3.remove(title_data)
+
+        for title_data in t4:
+            title_data[0] = (h / 12) * 9 + (Time - title_data[1]) * 350 * speed * (h/900)
+            pygame.draw.rect(screen, (255, 255, 255), (w /2 + w / 16, title_data[0] - h /100, w / 16, h / 50))
+            if title_data[0] > h - (h / 9):
+                t4.remove(title_data)
+
+
+        pygame.draw.rect(screen, (0, 0, 0), (w /2 - w / 8, (h /12 ) * 9, w / 4, h / 2))
+        pygame.draw.rect(screen, (255, 255, 255), (w /2 - w / 8, (h /12 ) * 9, w / 4, h / 2), int(h/100))
+
+        pygame.display.flip()
